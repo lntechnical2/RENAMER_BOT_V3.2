583d3bc8d6bdaac65560b0000e1a8ad6052a29bd
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-09-27 09:18:52 +0530

    Create about.py

---
+++ plugins/about.py
@@ -0,0 +1,6 @@
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.private & filters.command(["about"]))
+async def start(client,message):
+	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Heroku")
