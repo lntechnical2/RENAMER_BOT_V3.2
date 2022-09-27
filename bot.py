833572305c171913581b05ed966b9e7d6d9359de
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-09-27 08:56:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from pyrogram import Client
+import os
 
+TOKEN = os.environ.get("TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    app.run()
