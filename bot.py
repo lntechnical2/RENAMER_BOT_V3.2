57c90fd74c99e61572f299df71c6038b0fdd16cb
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-06 12:56:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
 from pyrogram import Client
 import os
 from plugins.cb_data import app
+
 TOKEN = os.environ.get("TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    bot = Client(
-        "renamer",
-        bot_token=TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins
-    )
-    bot.start()
-    app.start()
-    idle()
+STRING = os.environ.get("STRING", "")
+
+
+if STRING:
+        bot = Client(
+           "renamer",
+           bot_token=TOKEN,
+           api_id=API_ID,
+           api_hash=API_HASH,
+           plugins=plugins)
+        bot.start()
+        app.start()
+        idle()
+else:
+	bot = Client(
+           "renamer",
+           bot_token=TOKEN,
+           api_id=API_ID,
+           api_hash=API_HASH,
+           plugins=plugins) 
+	bot.run()
+	
