5df48e59ee96b364fc8c862f16c6dc29f88cfc32
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-06 17:02:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from pyrogram import Client
+import os
 
+feom plugins.cb_data import app
+TOKEN = os.environ.get("TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+STRING = os.environ.get("STRING", "")
+
+
+if STRING:
+        bot = Client(
+           "renamer",
+           bot_token=TOKEN,
+           api_id=API_ID,
+           api_hash=API_HASH,
+           plugins=dict(root='plugins'))
+        bot.run()
+        app.start()
+        
+        
+else:
+	bot = Client(
+           "renamer",
+           bot_token=TOKEN,
+           api_id=API_ID,
+           api_hash=API_HASH,
+           plugins=dict(root='plugins')) 
+	bot.run()
+Footer
