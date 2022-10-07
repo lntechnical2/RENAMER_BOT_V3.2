da3c3c7f67071d4c8692c743bff53bae3079ee5f
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-07 19:22:08 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -73,7 +73,7 @@ async def doc(bot,update):
      		c_time = time.time()
      		try:
      			filw = await app.send_document(log_channel,document = file_path,thumb=ph_path,caption = f"**{new_filename}**",progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
-     			await bot.copy_message(update.message.chat.id,log_channel,filw.id)
+     			await bot.copy_message(update.from_user.id,log_channel,filw.id)
      			
      			
      			
@@ -91,7 +91,7 @@ async def doc(bot,update):
      		c_time = time.time()
      		try:
      			filw = await app.send_document(log_channel,document = file_path,caption = f"**{new_filename}**",progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
-     			await bot.copy_message(update.message.chat.id,log_channel,filw.id)
+     			await bot.copy_message(update.from_user.id,log_channel,filw.id)
      			
      			await ms.delete()
      			os.remove(file_path)
