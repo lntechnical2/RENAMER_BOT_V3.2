a1f3c07095417cc99221f6c821c16c23201a9c7d
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-07 00:12:48 -0700

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -14,7 +14,7 @@ API_HASH = os.environ.get("API_HASH", "")
 
 STRING = os.environ.get("STRING", "")
 
-app = Client(STRING,API_ID, API_HASH,in_memory = True)
+app = Client("test", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
 @Client.on_callback_query(filters.regex('cancel'))
 async def cancel(bot,update):
