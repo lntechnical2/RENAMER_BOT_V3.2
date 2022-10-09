dbd9154894e75367c098ee645eb6cb21cade32eb
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-09 13:43:46 +0530

    Create admin.py

---
+++ plugins/admin.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from helper.premiumdb import add_premium 
+from helper.date import add_date
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+ 
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
+async def buypremium(bot, message):
+	id = message.text.split("/addpremium")
+	user_id = id[1].replace(" ", "")
+	date = add_date()
+	add_premium(user_id,date[0])
+	await message.reply_text("Added successfully",quote=True)
+		
