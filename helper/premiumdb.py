6af9d20cfd5d17248b3e638f53639e1bc8e3e49b
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-09 14:14:40 +0530

    Update premiumdb.py

--- helper/premiumdb.py
+++ helper/premiumdb.py
@@ -1,6 +1,6 @@
 import pymongo 
 import os
-
+from helper.date import add_date()
 DB_NAME = "premium"
 DB_URL = os.environ.get("DB_URL","")
 mongo = pymongo.MongoClient(DB_URL)
@@ -9,7 +9,8 @@ dbcol = db["user"]
 
 def add_premium(chat_id):
 	user_id = int(chat_id,date)
-	user_data = {"_id":user_id, "date":date}
+	date = add_date()
+	user_data = {"_id":user_id, "date":date[0]}
 	try:
 		dbcil.insert_one(user_data)
 	except:
