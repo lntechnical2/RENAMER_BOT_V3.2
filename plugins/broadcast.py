4488d5ab6cccf02b0c7a77697879bc59b37ba943
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-09 13:19:03 +0530

    Fixed Error

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,9 +4,10 @@ import asyncio
 from pyrogram import Client ,filters
 from helper.database import getid ,delete
 import time
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+ 
 
-
-@Client.on_message(filters.private & filters.user(923943045) & filters.command(["broadcast"]))
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
    ms = await message.reply_text("Geting All ids from database ...........")
