e58282ecbdea824f148d76275f1320a63b623495
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-23 08:38:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,7 +4,7 @@ import time
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 import humanize
-from helper.database import  insert ,find_one
+from helper.database import  insert ,find_one,used_limit,usertype,uploadlimit,addpredata
 from pyrogram.file_id import FileId
 
 from helper.premiumdb import find_one as findpr
@@ -54,54 +54,56 @@ async def send_doc(client,message):
        		[ [ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{update_channel}") ]   ]))
        		return
        
-       premi_check  = findpr(user_id)
        
-       _used_date = find_one(user_id)
-       used_date = _used_date["date"]
+       user_deta = find_one(user_id)
+       used_date = user_deta["date"]
+       try:
+           buy_date= user_deta["prexdate"]
+       except:
+           used_limit(user_id,0)
+           usertype(user_id,"Free")
+           uploadlimit(user_id,2147483648)
+           addpredata(user_id)        
+           await message.reply_text("Re Try .......... ")
+           return
+           
        c_time = time.time()
-       if premi_check:
-           LIMIT = 0
+       
+       if buy_date:
+           LIMIT = 50
        else:
            LIMIT = 350
        then = used_date+ LIMIT
        left = round(then - c_time)
        conversion = datetime.timedelta(seconds=left)
        ltime = str(conversion)
-       if left > 0:
-       	    
+       if left > 0:       	    
        	await message.reply_text(f"```Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}```",reply_to_message_id = message.id)
        else:
        		# Forward a single message
        		await client.forward_messages(log_channel, message.from_user.id, message.id)
+       		await client.send_message(log_channel,f"User Id :- {user_id}")       		
        		media = await client.get_messages(message.chat.id,message.id)
        		file = media.document or media.video or media.audio 
        		dcid = FileId.decode(file.file_id).dc_id
        		filename = file.file_name
-       		value = 2099999999
+       		value = 2147483648
        		if value < file.file_size:
-       		          if STRING:
-       		          	try:
-       		          		_used_date = findpr(user_id)
-       		          		buy_date = _used_date["date"]
-       		          		pre_check = check_expi(buy_date)
-       		          		if pre_check == True:
-       		          			await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {humanize.naturalsize(file.file_size)}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
-       		          		else:
-       		          			await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
-       		          			
-       		          	except Exception as e:
-       		          		await message.reply_text(f"You Can't Rename More Then 2GB file\nBuy Subscription\nOur plane\n* Paid Plane ₹ 25  INR For 26days\nPay using Upi ID ```lokamandc1224@oksbi```\nSend Screnshot To @mrlokaman",quote=True)
-       		          		return
-       		          	
-       		          	
-       		  
-       		          else:
+       		    if STRING:
+       		        if buy_date==None:
+       		            await message.reply_text(f"You Can't Rename More Then 2GB file\nBuy Subscription\nOur plane\n* Paid Plane ₹ 55  INR For 30days\nPay Using Paytm https://p.paytm.me/xCTH/los89jy0\nPay using Upi ID ```lokamandc1224@oksbi```\nSend Screnshot To @mrlokaman",quote=True)
+       		            return
+       		        pre_check = check_expi(buy_date)
+       		        if pre_check == True:
+       		            await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {humanize.naturalsize(file.file_size)}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
+       		        else:
+       		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
+       		    else:
        		          	await message.reply_text("Can't upload files bigger than 2GB ")
-       		          return
-       		filesize = humanize.naturalsize(file.file_size)
-       		fileid = file.file_id
-       		await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""",
-       		reply_to_message_id = message.id,
-       		reply_markup = InlineKeyboardMarkup(
+       		          	return
+       		else:
+       		    filesize = humanize.naturalsize(file.file_size)
+       		    fileid = file.file_id
+       		    await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup(
        		[[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),
        		InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
