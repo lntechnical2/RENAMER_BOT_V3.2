b8b62bbe43c9f3f2580aef7e653ca7b48d7190c6
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-23 07:10:17 +0530

    Create myplane.py

---
+++ plugins/myplane.py
@@ -0,0 +1,28 @@
+import time
+from pyrogram import Client, filters
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from function.database import  find_one 
+import datetime
+
+from function.progress import humanbytes
+
+
+@Client.on_message(filters.private & filters.command(["myplan"]))
+async def start(client,message):
+	used_ = find_one(message.from_user.id)
+	used = used_["used_limit"]
+	limit = used_["uploadlimit"]
+	remain = int(limit)- int(used)
+	user =  used_["usertype"]
+	ends = used_["prexdate"]
+	normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
+	if ends ==0:
+	    text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}"
+	else:
+	    text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}\n\n```Your Plan Ends On :- {normal_date}"
+	    
+	if user == "Free":
+	    await message.reply(text,quote = True,reply_markup = InlineKeyboardMarkup([[       			InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade"), InlineKeyboardButton("Cancel ✖️ ",callback_data = "cancel") ]]))
+	else:
+	    await message.reply(text,quote)
+	    
