95e6d6cea2b58363b4e959a01fd44c7ee76b6179
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-23 14:15:38 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,14 +1,36 @@
 import os
 from pyrogram import Client, filters
-from helper.premiumdb import add_premium 
 from helper.date import add_date
+from helper.database import uploadlimit , usertype,addpre
 ADMIN = int(os.environ.get("ADMIN", 923943045))
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+
  
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
 async def buypremium(bot, message):
-	id = message.text.split("/addpremium")
+	await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("VIP 1",callback_data = "vip1"), 
+        			InlineKeyboardButton("VIP 2",callback_data = "vip2") ]]))
+        			
+
+@Client.on_callback_query(filters.regex('vip1'))
+async def vip1(bot,update):
+	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	date = add_date()
-	add_premium(user_id)
-	await message.reply_text("Added successfully",quote=True)
-		
+	inlimit  = 10737418240
+	uploadlimit(int(user_id),10737418240)
+	usertype(int(user_id),"VIP1")
+	addpre(int(user_id))
+	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
+	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 1 check your plan here /myplan")
+
+@Client.on_callback_query(filters.regex('vip2'))
+async def vip2(bot,update):
+	id = update.message.reply_to_message.text.split("/addpremium")
+	user_id = id[1].replace(" ", "")
+	inlimit  = 53687091200
+	uploadlimit(int(user_id),53687091200)
+	usertype(int(user_id),"VIP2")
+	addpre(int(user_id))
+	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
+	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 2 check your plan here /myplan")
