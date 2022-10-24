81cbc0c957671fc6c88a60908645c22987c12f06
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-24 06:59:20 +0530

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -2,7 +2,7 @@ import time
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from helper.database import  find_one,used_limit 
-from helper.database import daily_ 
+from helper.database import daily as daily_ 
 import datetime
 from datetime import timedelta, date ,datetime
 from datetime import date as date_
