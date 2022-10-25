c7d095ca5d4e38c6dbc34538ce57c3c97ab6034d
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-10-25 21:04:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -58,10 +58,10 @@ async def start(client,message):
 	        except:
 	             return
 	    else:
-	         await client.send_message(id,"Congrats! You Won 50MB Upload limit")
+	         await client.send_message(id,"Congrats! You Won 100MB Upload limit")
 	         _user_= find_one(int(id))
-	         limit = used_["uploadlimit"]
-	         new_limit = limit + 52428800
+	         limit = _user_["uploadlimit"]
+	         new_limit = limit + 104857600
 	         uploadlimit(int(id),new_limit)
 	         await message.reply_text(text =f"""
 	Hello {wish} {message.from_user.first_name }
