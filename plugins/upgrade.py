c6a8123ae65399f7de40d167fefd4c4a3546dcae
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-11-01 08:57:09 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -33,7 +33,7 @@ async def upgrade(bot,update):
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
-async def upgradecm(bot,update):
+async def upgradecm(bot,message):
 	text = """**Free Plan User**
 	Daily  Upload limit 2GB
 	Price 0
@@ -59,4 +59,4 @@ async def upgradecm(bot,update):
         			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
         			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
         			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
+	await message.reply_text(text = text,reply_markup = keybord)
