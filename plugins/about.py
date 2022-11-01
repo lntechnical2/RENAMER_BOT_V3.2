9b6ec9930f3ced425eb674bdf95c15097ba1d45e
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-11-01 18:22:28 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,13 @@
+import os 
 from pyrogram import Client, filters
-
-
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
+from helper.database import botdata, find_one
+from helper.progress import humanbytes
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
-	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway")
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
+	await message.reply_text(f"Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\n Total Renamed File :-{total_rename}\n Total Size Renamed :- {humanbytes(total_size)} ",quote=True)
