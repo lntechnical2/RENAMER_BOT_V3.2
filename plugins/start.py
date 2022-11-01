64b1971af2a983d7ab45820f9b2c5c03cbc8703a
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-11-01 18:45:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -91,7 +91,7 @@ async def send_doc(client,message):
        		[ [ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{update_channel}") ]   ]))
        		return
        
-       bot_data = find_one(botid)
+       bot_data = find_one(int(botid))
        prrename = bot_data['total_rename']
        prsize = bot_data['total_size']
        user_deta = find_one(user_id)
