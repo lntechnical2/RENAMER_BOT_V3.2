db621798270fc9d2c1ec6d5399d53d155b012109
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-12-10 21:05:46 +0530

    Fix 😕

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -9,33 +9,34 @@ ADMIN = int(os.environ.get("ADMIN", 923943045))
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
- if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
-   ids = getid()
-   tot = len(ids)
-   failed = 0 
-   success = 0
-   startsm = time.time()
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-   for id in ids:
-       roress = startsm - time.time()
-       if (round(roress % 6.00) == 0):
-           try:
-               await ms.edit(text=f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
-           except:
-               pass
-       try:
-     	   await message.reply_to_message.copy(id)
-           success += 1 
-       except:
-     	    failed += 1
+
+    if (message.reply_to_message):
+        ms = await message.reply_text("Geting All ids from database ...........")
+        ids = getid()
+        tot = len(ids)
+        failed = 0 
+        success = 0
+        startsm = time.time()
+        await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+        for id in ids:
+            roress = startsm - time.time()
+            if (round(roress % 6.00) == 0):
+                try:
+                    await ms.edit(text=f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
+                except:
+                    pass
             try:
-     	        delete({"_id":id})
-            except:  	 
-     	        pass
-   try:
-       await ms.edit(text=f"BROADCST COMPLETED\n\nSUCCESS : {success}\nFAILED : {failed}")
-   except:
-       pass
- else:
-     await message.reply_text("Reply to any message to start broadcast")
+     	        await message.reply_to_message.copy(id)
+                success += 1 
+            except:
+     	        failed += 1
+                try:
+     	            delete({"_id":id})
+                except:  	 
+     	            pass
+        try:
+            await ms.edit(text=f"BROADCST COMPLETED\n\nSUCCESS : {success}\nFAILED : {failed}")
+        except:
+            pass
+    else:
+        await message.reply_text("Reply to any message to start broadcast")
