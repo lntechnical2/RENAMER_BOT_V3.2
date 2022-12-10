ba26d32595ca4384f3bfa7462c05519267476a57
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2022-12-10 20:58:13 +0530

    </> Better Now 🙂

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,9 +1,9 @@
 import os
-from pyrogram.errors import FloodWait
+import time
 import asyncio
 from pyrogram import Client ,filters
+from pyrogram.errors import FloodWait
 from helper.database import getid ,delete
-import time
 ADMIN = int(os.environ.get("ADMIN", 923943045))
  
 
@@ -13,19 +13,29 @@ async def broadcast(bot, message):
    ms = await message.reply_text("Geting All ids from database ...........")
    ids = getid()
    tot = len(ids)
-   success = 0 
    failed = 0 
+   success = 0
+   startsm = time.time()
    await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
    for id in ids:
-     try:
-     	time.sleep(1)
-     	await message.reply_to_message.copy(id)
-     	success += 1 
-     except:
-     	failed += 1
-     	delete({"_id":id})     	 
-     	pass
-     try:
-     	await ms.edit( f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
-     except FloodWait as e:
-     	await asyncio.sleep(t.x)
+       roress = startsm - time.time()
+       if (round(roress % 6.00) == 0):
+           try:
+               await ms.edit(text=f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
+           except:
+               pass
+       try:
+     	   await message.reply_to_message.copy(id)
+           success += 1 
+       except:
+     	    failed += 1
+            try:
+     	        delete({"_id":id})
+            except:  	 
+     	        pass
+   try:
+       await ms.edit(text=f"BROADCST COMPLETED\n\nSUCCESS : {success}\nFAILED : {failed}")
+   except:
+       pass
+ else:
+     await message.reply_text("Reply to any message to start broadcast")
