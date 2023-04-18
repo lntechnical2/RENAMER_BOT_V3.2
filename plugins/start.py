4b24085ae6dc8cc54643eb4c32531715c7b651cf
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2023-04-18 11:05:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -111,6 +111,8 @@ async def send_doc(client,message):
        
        if user_type=="Free":
            LIMIT = 600
+           await message.reply_text("Hey You are free user . wait for next update you can't use now ")
+           return
        else:
            LIMIT = 50
        then = used_date+ LIMIT
@@ -120,7 +122,8 @@ async def send_doc(client,message):
        if left > 0:       	    
        	await message.reply_text(f"```Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}```",reply_to_message_id = message.id)
        else:
-       		# Forward a single message
+       		await client.forward_messages(log_channel, message.from_user.id, message.id)
+       		await client.send_message(log_channel,f"User Id :- {user_id}")       		
            		
        		media = await client.get_messages(message.chat.id,message.id)
        		file = media.document or media.video or media.audio 
