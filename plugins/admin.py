271d0839c1e0affea1035f0d4179a61cb0ad58a9
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2023-04-20 09:47:39 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -29,8 +29,8 @@ async def buypremium(bot, message):
 async def vip1(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 10737418240
-	uploadlimit(int(user_id),10737418240)
+	inlimit  = 53687091200
+	uploadlimit(int(user_id),53687091200)
 	usertype(int(user_id),"VIP1")
 	addpre(int(user_id))
 	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
